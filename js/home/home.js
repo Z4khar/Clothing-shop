af48a0f44273d57bcb71122bc4a6421ab0521528
Z4khar <zahar.potaenko@mail.ru>
2025-11-14 17:32:02 +0300

    Fix links for GitHub Pages

--- js/home/home.js
+++ js/home/home.js
@@ -3,6 +3,6 @@ const btn_basket = document.querySelector('.btn-basket');
 btn_basket.addEventListener('click', () => location_cart());
 
 function location_cart() {
-    window.location.href = '../../basket.html';
+    window.location.href = 'basket.html';
     console.log('clicked');
 }
